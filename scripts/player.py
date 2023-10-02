490358f4c26b071e058882206049a97f3b53c060
Eliezer Alves <eliezer.c.alves2015@gmail.com>
2023-10-02 07:51:00 -0300

    movimentação do player

--- scripts/player.py
+++ scripts/player.py
@@ -1,4 +1,5 @@
 import pygame
+from scripts.settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collison_group):
@@ -7,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
         self.collision_group = collison_group
         self.direction = pygame.math.Vector2()
-        self.speed = 0.1
+        self.speed = 5
         self.jump_force = 10
         self.gravity = 0.2
         self.on_ground = False
@@ -28,12 +29,16 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.input()
-        print("update da player")
-        # self.move()
-        # self.gravity_force()
-        # self.y_collison()
+        self.move()
+        self.gravity_force()
+        self.y_collison()
 
     def y_collison(self):
+        if self.rect.y >= HEIGHT - 100:
+            self.direction.y = 0
+            self.rect.y = HEIGHT - 100
+            self.on_ground = True
+
         for sprite in self.collision_group:
             if sprite.rect.colliderect(self.rect):
                 if self.direction.y > 0:
@@ -51,14 +56,15 @@ class Player(pygame.sprite.Sprite):
 
     def input(self):
         key = pygame.key.get_pressed()
-        if key[pygame.K_a]:
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
             self.direction.x = -1
             self.flip = True
             self.animation(8, 3, "assets/player/walk_")
-        elif key[pygame.K_d]:
+        elif key[pygame.K_d] or key[pygame.K_RIGHT]:
             self.direction.x = 1
             self.flip = False
             self.animation(8, 3, "assets/player/walk_")
+            print("Direita")
         else:
             self.direction.x = 0
             self.animation(16, 2, "assets/player/walk_")
